b97df442708dea7b537ea15bd0561988d1f54d47
Richard Thier <u9vata@gmail.com>
2018-12-30 19:52:02 +0000

    Added endl to logging so we see the calls. /factor seems to work and is fast

--- src/lightcontrol.c
+++ src/lightcontrol.c
@@ -6,7 +6,7 @@ static void factor(Request * request, Response * response);
 
 void server(Request * request, Response * response)
 {
-	printf("%s %s", request->method, request->reqStr);
+	printf("%s %s\n", request->method, request->reqStr);
 	routeRequest(request, response, "/factor", factor);
 }
 
