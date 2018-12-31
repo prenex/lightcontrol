784a6c099936c8bb124bdf48ba31c6b9d6ccf057
Richard Thier <u9vata@gmail.com>
2018-12-31 00:20:46 +0000

    Added index.html, main.css and their endpoint routes in the server.

--- src/lightcontrol.c
+++ src/lightcontrol.c
@@ -2,16 +2,35 @@
 #include "waferapi.h"
 #include <string.h>
 
-static void factor(Request * request, Response * response);
+static void factor(Request *request, Response *response);
+static void index_html(Request *request, Response *response);
+static void main_css(Request *request, Response *response);
 
-void server(Request * request, Response * response)
-{
-	printf("%s %s\n", request->method, request->reqStr);
+void server(Request *request, Response *response) {
+	printf("%s %s \n", request->method, request->reqStr);
+	/* TODO: this becomes a long row of ifs even if one succeeds - some should be maybe put in else clauses for speed if needed. */
+	routeRequest(request, response, "", index_html);
+	routeRequest(request, response, "/", index_html);
+	routeRequest(request, response, "/index.html", index_html);
+	routeRequest(request, response, "/main.css", main_css);
 	routeRequest(request, response, "/factor", factor);
 }
 
-static void factor(Request * request, Response * response)
-{
+/* index.html */
+static void index_html(Request *request, Response *response) {
+	/* Serve the file directly */
+	/* If the second param would be a filename the browser would download the document */
+	serveFile(request, response, "index.html", NULL, "text/html");
+}
+
+/* main.css */
+static void main_css(Request *request, Response *response) {
+	/* Serve the file directly */
+	/* If the second param would be a filename the browser would download the document */
+	serveFile(request, response, "main.css", NULL, "text/html");
+}
+
+static void factor(Request *request, Response *response) {
 	char *nStr = QUICK_FORM_TEXT(request, response, "Number to factor:");
 
 	if (nStr != NULL) {
